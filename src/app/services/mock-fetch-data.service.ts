4d1b5fb3678b28aea3925e725fe3a7c2588db67a
Erik Målsäter <erik.malsater92@gmail.com>
2019-04-30 16:12:49 +0200

    Created mock data for mock fetch-data-service

--- src/app/services/mock-fetch-data.service.ts
+++ src/app/services/mock-fetch-data.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { IFetchDataService } from '../interfaces/IFetchDataService';
+import { IProduct } from '../interfaces/IProduct';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MockFetchDataService {
+export class MockFetchDataService implements IFetchDataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  mockProducts = [
+    { name: "The Shining", price: 139, year: 1978 },
+    { name: "Pulp Fiction", price: 159, year: 1994 },
+    { name: "Kill Bill", price: 199, year: 2001 }
+  ]
+
+  getData(): IProduct[] {
+    return this.mockProducts;
+  }
+
 }
