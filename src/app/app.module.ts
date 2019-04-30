80f2b09fa592a7a834d11d49341628f69122aa44
Erik Målsäter <erik.malsater92@gmail.com>
2019-04-30 13:49:02 +0200

    Created component feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
