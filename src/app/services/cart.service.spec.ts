7170ec3c2c3761d809c11baa2a449df5bfe612de
Erik Målsäter <erik.malsater92@gmail.com>
2019-05-21 14:04:54 +0200

    Add styling to cart

--- src/app/services/cart.service.spec.ts
+++ src/app/services/cart.service.spec.ts
@@ -49,28 +49,13 @@ describe('CartService', () => {
 
       const mock: MockAddProductToCartService = TestBed.get(MockAddProductToCartService);
       sessionStorage.setItem("productCart", JSON.stringify([mock.getMockData()]));
-/*
-      sessionStorage.setItem("productCart", JSON.stringify([{
-        "id": 1, 
-        "name": "The Shining", 
-        "price": 139, 
-        "year": 1978, 
-        "imageUrl": "https://upload.wikimedia.org/wikipedia/en/e/ea/The_Shining_%281980%29.png",
-        "description": "Once upon a time.",
-        "productCategory": [{"categoryId": 1}, {"category": null}]
-        }]));
-      */
+
     });
 
     it('should fetch 1 object from session storage and return it', () => {
       const service: CartService = TestBed.get(CartService);
-      
-      //const mock: MockFetchDataService = TestBed.get(MockFetchDataService);
-      
 
       let cart = service.fetchCart();
-      console.log(cart);
-      //let cart = JSON.parse(sessionStorage.getItem("productCart"));
       expect(cart.length).toEqual(1);
     });
   });
